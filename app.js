c8152f2a79ebb6600465c3712b3a25f20bbe9e34
Alicia Centeno <alicent.08.02@gmail.com>
2018-05-28 13:30:17 -0400

    correcion

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
+
 // Preguntando edad del usuario.
-const age = parseInt(prompt("¿Cual es tu edad?"));
+
+
+let isInt = function (n) { return parseInt(n) === n };
+
+do {
+	let age = parseInt(prompt("¿Cual es tu edad?"));
+	 
+} while (!isInt === age);
+ 
+   alert("¡Error!");
+
 
 //ingreso de datos del cliente
-if ( age <= 3 && age >= 1 ) {
+if (age >= 1 && age <= 3  ) {
 	alert("Por tu edad estas clasificado en toddler");
 
-   } else if ( age <= 5  && age >= 12 ) {
+   } else if (age <= 5  && age >= 12) {
 	   alert("Por tu edad estas clasificado en preschooler");
 
- } else if ( age <= 12 && age >= 5 ){
+ } else if (age >= 5 && age <= 12){
          alert("Por tu edad estas clasificado en gradeschooler");
 
-     }else if ( age <= 18 && age >= 12 ) {
+     }else if (age >= 12 && age <= 18) {
 		    alert("Por tu edad estas clasificado en teenager");
 		
-      } else if ( age <= 21 && age >= 18 ) {
+      } else if (age >= 18 && age <= 21) {
 	alert("Por tu edad estas clasificado en young adult ");
 				 
-} else if (age <= 60 && age >= 21) {
-	alert("Por tu edad estas clasificado en adult")
+} else{(age >= 22 && age <= 60);
+	alert("Por tu edad estas clasificado en adult");
 	
 }
   
